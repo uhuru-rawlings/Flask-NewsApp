afc3768d511797c582cbedfe2f737044107bf519
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-01-31 10:01:18 +0300

    changed date for the url

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
+from flask import Flask
 from .config import DevConfig
-from flask_bootstrap import Bootstrap
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
@@ -8,5 +8,5 @@ app = Flask(__name__,instance_relative_config = True)
 # Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
-bootstrap = Bootstrap(app)
+
 from app import views
--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 class Config:
 
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/everything?q={}&from=2021-12-29&sortBy=publishedAt&apiKey={}'
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/everything?q={}&from=2021-12-31&sortBy=publishedAt&apiKey={}'
     # NEWS_API_KEY = 'bc0e9653f5b94bb884afc47e3704ebb6'
 
 class ProdConfig(Config):
